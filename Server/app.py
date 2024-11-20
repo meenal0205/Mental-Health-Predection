53d0f9630e3135ae5adf27277a0c45ab0cdfb174
meenal0205 <meenalbharsakle123@gmail.com>
2024-11-20 23:25:08 +0530

    mongo setup

---
+++ Server/app.py
@@ -0,0 +1,87 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pickle
+import numpy as np
+from scipy.sparse import hstack
+import re
+from flask import Flask, request,Response, redirect
+import requests 
+
+from flask import Flask, jsonify, request, session
+from flask_pymongo import PyMongo
+from werkzeug.utils import secure_filename
+from pymongo import MongoClient
+from gridfs import GridFS
+from flask_cors import CORS
+import os
+from datetime import datetime
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = str(os.getenv('MONGO_URI'))
+
+mongo = PyMongo(app)
+CORS(app)
+
+client = MongoClient(str(os.getenv('MONGO_URI')))
+db = client.get_database()
+fs = GridFS(db)
+
+
+
+
+with open("C:/Major Project/Server/TfidfVectorizer.pkl", 'rb') as f:
+    vectorizer = pickle.load(f)
+
+with open("C:/Major Project/Server/LogisticModel.pkl", 'rb') as file:  
+    model = pickle.load(file)
+
+
+def remove_patterns(text):
+    text = re.sub(r'http[s]?://\S+', '', text)
+    text = re.sub(r'\[.*?\]\(.*?\)', '', text)
+    text = re.sub(r'@\w+', '', text)
+    text = re.sub(r'[^\w\s]', '', text)
+    return text.strip()
+
+
+def generate(sentence):
+    sentence_tfidf = vectorizer.transform([sentence])
+    num_of_characters = len(sentence)
+    num_of_sentences = sentence.count('.') + sentence.count('!') + sentence.count('?')  
+    sentence_num = np.array([[num_of_characters, num_of_sentences]])
+    sentence_combined = hstack([sentence_tfidf, sentence_num])
+    labels = {0: 'Anxiety', 1: 'Bipolar', 2: 'Depression', 3: 'Normal', 4: 'Personality disorder', 5: 'Stress', 6: 'Suicidal'}
+    y_predict = model.predict(sentence_combined)
+    predicted_label = labels[y_predict[0]]
+    return predicted_label
+
+
+
+@app.route("/create-diary-entry", methods=['POST'])
+def createEntry():
+    data = request.get_json()
+    diary_title=data['title']
+    user_id=data['user']
+    diary_entry= data['diary_entry']
+    mongo.db.reports.insert_one({'userId':user_id,'title':diary_title,'content':diary_entry,'created_at':datetime.today().replace(microsecond=0),'updated_at': datetime.today().replace(microsecond=0)})
+
+    return generate(diary_title+diary_entry)
+
+
+
+    
+
+
+
+
+    
+
+
+    
+
+
+
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
